83fed14abd6ce5386f83cd478b5afff5975023c1
Ismail Ariyan <ariyan.git.8hxlz@aleeas.com>
2023-05-16 13:04:06 +0600

    done adding the cell option

--- src/shopms/CustomerForm.java
+++ src/shopms/CustomerForm.java
@@ -16,7 +16,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ResourceBundle;
-/gg
+//gg
 
 public class CustomerForm implements Initializable {
     @FXML
